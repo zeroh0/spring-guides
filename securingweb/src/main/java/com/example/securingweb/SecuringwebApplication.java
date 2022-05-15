1f36aa8a05d3dbe627269be861c678e8109bfc43
zeroh0 <umberto1900@naver.com>
2022-05-15 14:11:18 +0900

    Securing a Web Application

---
+++ securingweb/src/main/java/com/example/securingweb/SecuringwebApplication.java
@@ -0,0 +1,14 @@
+package com.example.securingweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/* Run the Application */
+@SpringBootApplication
+public class SecuringwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringwebApplication.class, args);
+	}
+
+}
