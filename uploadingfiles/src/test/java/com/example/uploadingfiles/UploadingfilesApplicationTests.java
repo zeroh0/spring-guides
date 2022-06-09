579fc2b7e15c9916604c852822d55e7b35189480
zeroh0 <umberto1900@naver.com>
2022-06-09 23:37:41 +0900

    Uploading Files

---
+++ uploadingfiles/src/test/java/com/example/uploadingfiles/UploadingfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uploadingfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadingfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
